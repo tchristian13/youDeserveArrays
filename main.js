96be0e6273edcafc9d8b9fc7a45464cd8127508e
tchristian13 <tchrstn4@gmail.com>
2025-04-17 03:29:29 -0500

    commit

---
+++ main.js
@@ -0,0 +1,52 @@
+/******************
+ * YOUR CODE HERE *
+ ******************
+
+
+
+// //const {
+//   getLastItemFrom,
+//   getIndex3,
+//   isLongList,
+//   firstItemIsNumber,
+//   secondCharOfThirdString,
+
+// }
+
+  function getFirstItemFrom [veggies]{}
+  return veggies[0];
+    
+  {
+
+
+   const getLastItemFrom (veggies) {
+     return  (firstItem = veggies.length-1)
+    }
+
+   getIndex3 (veggies) {
+    return (veggies(3)[0] || return veggies.length - 1)
+    }
+
+   isLongList (numArr) {
+   if (numArr.length === 0){
+   return false;
+   } else {
+    
+   if (typeof numArr[0] ===  'number' &&  numArr.length >= 10){
+      return true
+      }
+
+   firstItemIsNumber (num) 
+
+   const numAr=['22', '54', 'Tia', 'false']{
+     if (typeof num[0] === number)
+            }
+     return true;
+     
+
+     secondCharOThirdString (fruits)   
+    const MyArr=["apples", "oranges", "bananas"]{
+}
+     return  MyArr(2)[3];
+    console.log (fruits(2)[3])
+        
